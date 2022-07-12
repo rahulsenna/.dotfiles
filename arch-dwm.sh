62eb2be1418e55d65d9afb8abcfcbcacb52242a2
Rahul Senna <rahulvsenna@gmail.com>
2022-07-12 06:45:21 +0000

    update

--- arch-dwm.sh
+++ arch-dwm.sh
@@ -1,76 +1,45 @@
 #!/bin/bash
 
-# Variables
-country=Poland
-kbmap=ch
-output=Virtual-1
-resolution=1920x1080
-
-# Options
-aur_helper=true
-install_ly=true
-gen_xprofile=false
-
-sudo timedatectl set-ntp true
-sudo hwclock --systohc
-sudo reflector -c $country -a 12 --sort rate --save /etc/pacman.d/mirrorlist
-
-# sudo firewall-cmd --add-port=1025-65535/tcp --permanent
-# sudo firewall-cmd --add-port=1025-65535/udp --permanent
-# sudo firewall-cmd --reload
-# sudo virsh net-autostart default
-
-if [[ $aur_helper = true ]]; then
-    cd /tmp
-    git clone https://aur.archlinux.org/paru.git
-    cd paru/;makepkg -si --noconfirm;cd
-fi
 
 # Install packages
-sudo pacman -S xorg firefox polkit-gnome nitrogen lxappearance thunar
+sudo pacman -S xorg-server xorg-xinit xorg-xrandr xorg-xsetroot chromium nitrogen picom zsh zsh-completions alacritty ranger pcmanfm exa breeze-gtk alsa-utils pamixer playerctl pulseaudio
 
 # Install fonts
-sudo pacman -S --noconfirm dina-font tamsyn-font bdf-unifont ttf-bitstream-vera ttf-croscore ttf-dejavu ttf-droid gnu-free-fonts ttf-ibm-plex ttf-liberation ttf-linux-libertine noto-fonts ttf-roboto tex-gyre-fonts ttf-ubuntu-font-family ttf-anonymous-pro ttf-cascadia-code ttf-fantasque-sans-mono ttf-fira-mono ttf-hack ttf-fira-code ttf-inconsolata ttf-jetbrains-mono ttf-monofur adobe-source-code-pro-fonts cantarell-fonts inter-font ttf-opensans gentium-plus-font ttf-junicode adobe-source-han-sans-otc-fonts adobe-source-han-serif-otc-fonts noto-fonts-cjk noto-fonts-emoji
+sudo pacman -S --noconfirm ttf-jetbrains-mono ttf-font-awesome
 
-# Pull Git repositories and install
-cd /tmp
-repos=( "dmenu" "dwm" "dwmstatus" "st" "slock" )
+# DWM
+mkdir .suckless
+cd .suckless;
+repos=( "dmenu" "dwmstatus" "st" "slock" )
 for repo in ${repos[@]}
 do
     git clone git://git.suckless.org/$repo
     cd $repo;make;sudo make install;cd ..
 done
 
-# XSessions and dwm.desktop
-if [[ ! -d /usr/share/xsessions ]]; then
-    sudo mkdir /usr/share/xsessions
-fi
+git clone https://github.com/rahulsenna/dwm.git
+cd dwm;make;sudo make install;cd ..
 
-cat > ./temp << "EOF"
-[Desktop Entry]
-Encoding=UTF-8
-Name=Dwm
-Comment=Dynamic window manager
-Exec=dwm
-Icon=dwm
-Type=XSession
-EOF
-sudo cp ./temp /usr/share/xsessions/dwm.desktop;rm ./temp
 
-# Install ly
-if [[ $install_ly = true ]]; then
-    git clone https://aur.archlinux.org/ly
-    cd ly;makepkg -si
-    sudo systemctl enable ly
-fi
+# Change shell to Zsh
+chsh -s /bin/zsh
 
-# .xprofile
-if [[ $gen_xprofile = true ]]; then
-cat > ~/.xprofile << EOF
-setxkbmap $kbmap
-nitrogen --restore
-xrandr --output $output --mode $resolution
-EOF
-fi
+./link
 
-printf "\e[1;32mDone! you can now reboot.\e[0m\n"
+# Yay
+sudo pacman -S --needed base-devel git
+git clone https://aur.archlinux.org/yay.git
+cd yay;makepkg -si;cd ..
+
+#SublimeText
+sudo pacman -Syu haveged
+sudo systemctl start haveged
+sudo systemctl enable haveged
+sudo rm -fr /etc/pacman.d/gnupg
+sudo pacman-key --init
+sudo pacman-key --populate archlinux
+
+
+curl -O https://download.sublimetext.com/sublimehq-pub.gpg && sudo pacman-key --add sublimehq-pub.gpg && sudo pacman-key --lsign-key 8A8F901A && rm sublimehq-pub.gpg
+echo -e "\n[sublime-text]\nServer = https://download.sublimetext.com/arch/stable/x86_64" | sudo tee -a /etc/pacman.conf
+sudo pacman -Syu sublime-text
